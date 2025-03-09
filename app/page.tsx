a1c032b8daa12d69a1d846c20da8b99d999547c2
Tadeusz Kielak <tadeusz@kielak.com>
2025-03-09 09:58:34 +0000

    zmiana testowa

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
 export default function Home() {
-    return <h1 className="text-3xl font-bold text-blue-600">Braggly! 🚀</h1>;
+    return <h1 className="text-3xl font-bold text-blue-600">Braggly!!! 🚀</h1>;
   }
   
