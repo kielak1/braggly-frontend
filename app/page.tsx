da80f07859b48a0fa0848e7a48ea1dfdc012c546
Tadeusz Kielak <tadeusz@kielak.com>
2025-03-10 18:55:27 +0000

    definicja adresu backend

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,13 @@ export default async function Home() {
   const cookieStore = await cookies();
   const locale = cookieStore.get("locale")?.value || "es"; // Domyślnie angielski
   const translations = await getTranslations(locale);
-
+  const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
   return (
     <div>
+      <h1>Backend URL: {backendUrl}</h1>;
       <h1 className="text-3xl font-bold text-blue-600">Braggly!!! 🚀</h1>
       <h1>{translations.greeting || "Loading..."}</h1>
       <p>{translations.welcome}</p>
     </div>
-  );
+  );s
 }
