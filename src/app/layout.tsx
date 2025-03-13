2c719057288d2a7dcbef373885ae8a21e26f6523
Tadeusz Kielak <tadeusz@kielak.com>
2025-03-13 21:36:12 +0000

    struktura plikow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import "../styles/globals.css"; // Import Tailwind CSS
+import '@/styles/globals.css';
 import { SessionProvider } from "next-auth/react";
-import LanguageSwitcher from "@/components/LanguageSwitcher";
 import Navbar from "@/components/Navbar";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
