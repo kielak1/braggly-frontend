0b2c4ee3adb09fe1870ccfe7a8ab3c0fe29e20c5
Tadeusz Kielak <tadeusz@kielak.com>
2025-03-16 20:58:18 +0000

    naigacjka pomiedzy root user i admin

---
+++ src/app/user/page.tsx
@@ -0,0 +1,9 @@
+
+export default function AdminPage() {
+    return (
+      <div>
+        <h1 className="text-xl font-bold">Panel Użytkownika</h1>
+        <p>Witaj w panelu użytkownika!</p>
+      </div>
+    );
+  }
