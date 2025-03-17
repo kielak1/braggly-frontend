ccd533792b59de28d9013ad0f384cd4c7906e685
Tadeusz Kielak <tadeusz@kielak.com>
2025-03-17 21:56:15 +0000

    stylizacja navbar i strony startowej

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   const translations = await getTranslations(locale);
 
   return (
-    <div className="relative min-h-[calc(100vh-64px)]">
+    <div className="relative min-h-[calc(100vh-64px)] flex items-center justify-center">
       {/* Obrazek jako tło */}
       <Image
         src="/braggly_xrd_graphic.png"
@@ -20,9 +20,9 @@ export default async function Home() {
       />
 
       {/* Tekst na tle */}
-      <div className="relative flex flex-col items-center justify-center h-full bg-black/60 text-white text-center px-6 py-10 rounded-lg backdrop-blur-md max-w-2xl mx-auto">
+      <div className="relative flex flex-col items-center justify-center bg-black/60 text-white text-center px-6 py-10 rounded-lg backdrop-blur-md max-w-2xl">
         <h1 className="text-5xl font-extrabold bg-gradient-to-r from-yellow-400 to-orange-500 bg-clip-text text-transparent">
-          Braggly!!!
+          Braggly !!!
         </h1>
         <p className="text-2xl mt-4">{translations.greeting || "Loading..."}</p>
         <p className="text-lg mt-2">{translations.welcome}</p>
