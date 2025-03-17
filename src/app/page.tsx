91102eeb816fc7034c4d014975ea6dbb649d2213
Tadeusz Kielak <tadeusz@kielak.com>
2025-03-17 21:41:02 +0000

    strona startowa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,26 +8,25 @@ export default async function Home() {
   const translations = await getTranslations(locale);
 
   return (
-    <div >
+    <div className="relative min-h-[calc(100vh-64px)]">
       {/* Obrazek jako tło */}
       <Image
         src="/braggly_xrd_graphic.png"
         alt="XRD Spectroscopy Graph"
-        width={1920}
-        height={1080}
-      //  objectFit="cover"
+        fill
+        style={{ objectFit: "cover" }}
         priority
-     //   className="absolute top-0 left-0 w-full h-full z-[-1]"
+        className="z-[-1]"
       />
 
-      {/* Tekst na tle
-      <div className="relative flex flex-col items-center justify-center bg-black/60 text-white text-center px-6 py-10 rounded-lg backdrop-blur-md max-w-2xl">
+      {/* Tekst na tle */}
+      <div className="relative flex flex-col items-center justify-center h-full bg-black/60 text-white text-center px-6 py-10 rounded-lg backdrop-blur-md max-w-2xl mx-auto">
         <h1 className="text-5xl font-extrabold bg-gradient-to-r from-yellow-400 to-orange-500 bg-clip-text text-transparent">
           Braggly!!!
         </h1>
         <p className="text-2xl mt-4">{translations.greeting || "Loading..."}</p>
         <p className="text-lg mt-2">{translations.welcome}</p>
-      </div> */}
+      </div>
     </div>
   );
 }
