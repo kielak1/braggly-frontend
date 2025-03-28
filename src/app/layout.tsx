49445dab87b5e8c79f008d702b7daa216bf75d5d
Tadeusz Kielak <tadeusz@kielak.com>
2025-03-28 20:49:13 +0000

    Google Anal;ytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
 import "@/styles/globals.css";
 import { Metadata } from "next";
+import Script from "next/script"; // dodaj to
 import ClientLayout from "./ClientLayout";
 import Foot from "@/components/Foot";
+
+const isProduction = process.env.NODE_ENV === "production";
+const GA_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
 
 export const metadata: Metadata = {
   title: "Braggly App",
@@ -9,9 +13,32 @@ export const metadata: Metadata = {
   manifest: "/manifest.json",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
+      <head>
+        {isProduction && GA_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="ga-init" strategy="afterInteractive">
+              {`
+                console.log("GA init running on page load");
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_ID}');
+              `}
+            </Script>
+          </>
+        )}
+      </head>
       <body>
         <ClientLayout>
           {children}
